d6be6b13babc98953a1f7c1f111d0005da3ee9c1
Алла Королева <anet.sergeevna.89@mail.ru>
2025-04-05 21:04:59 +0300

    add index.html file

--- js/main.js
+++ js/main.js
@@ -2,22 +2,10 @@
 function changeLanguage(lang) {
     const translations = {
         ru: {
-            "header-1": "Мой Блог",
-            "post-title-1": "Первая Запись Блога",
-            "post-excerpt-1": "Это краткий отрывок первой записи блога.",
-            "read-more": "Читать далее",
-            "single-post-title": "Первая Запись Блога",
-            "single-post-content": "Это полный текст первой записи блога.",
-            "back-to-home": "Вернуться на главную"
+            
         },
         en: {
-            "blog-title": "My Blog",
-            "post-title-1": "First Blog Post",
-            "post-excerpt-1": "This is a short excerpt of the first blog post.",
-            "read-more": "Read More",
-            "single-post-title": "First Blog Post",
-            "single-post-content": "This is the full content of the first blog post.",
-            "back-to-home": "Back to Homepage"
+            
         }
     };
 
